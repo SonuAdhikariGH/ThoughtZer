d2410aad980305803edad7dfa0a19584948e227f
Sonu Adhikari <sonuadhikarionly@gmail.com>
2026-04-04 22:16:23 +0530

    Updated the UI a bit while also worked on adding a save quote button that will save the quote in realtime to your designated local storage, Fixed some bugs in previous release and version history is under development thereafter.

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
+const quoteArr = JSON.parse(localStorage.getItem("quoteArr")) || [];
 const thoughtMain = document.querySelector("#thoughtMain");
 const authorName = document.querySelector("#authorName");
 const generateThoughtBtn = document.querySelector("#generateThoughtBtn");
+const preQuoteText = document.querySelector("#preQuoteText");
+const btnContainer = document.querySelector("#btnContainer");
+const quoteLi = document.querySelector("#quoteLi");
+const preSkeleton = document.querySelector("#preSkeleton");
 
 // btn event handler
 async function generateThoughtHandler(e) {
   e.preventDefault();
 
-  thoughtMain.textContent = "Loading..";
+  thoughtMain.textContent = "Fetching Wisdom...";
   authorName.textContent = "";
+  preQuoteText.textContent = "";
 
   try {
     const res = await fetch("https://dummyjson.com/quotes/random");
@@ -16,10 +22,62 @@ async function generateThoughtHandler(e) {
     thoughtMain.textContent = data.quote;
     authorName.textContent = `~${data.author}`;
     generateThoughtBtn.innerHTML = `Refresh`;
-  } catch {
-    thoughtMain.textContent(`404 Error`);
-    throw new console.error(`Server Didn't Respond, Please Try Again`);
+  } catch (err) {
+    console.error(err);
   }
 }
 
+let saveQuoteBtn = document.querySelector(".saveQuoteBtn");
+
+if (!saveQuoteBtn) {
+  saveQuoteBtn = document.createElement("button");
+  saveQuoteBtn.className = "saveQuoteBtn";
+  saveQuoteBtn.innerHTML = `Save Quote`;
+  btnContainer.appendChild(saveQuoteBtn);
+}
+
+// saveQuoteBtn event handler
+saveQuoteBtn.addEventListener("click", (e) => {
+  preSkeleton.textContent = "";
+  e.preventDefault();
+  let quote = {
+    quote: thoughtMain.textContent,
+    author: authorName.textContent.replace("~", ""),
+    date: new Date().toLocaleDateString(),
+  };
+
+  quoteArr.push(quote);
+
+  localStorage.setItem("quoteArr", JSON.stringify(quoteArr));
+  renderQuotes();
+});
+
+// renderQuotes function
+function renderQuotes() {
+  if (quoteArr.length > 0) {
+    preSkeleton.textContent = "";
+  }
+  quoteLi.innerHTML = "";
+
+  quoteArr.forEach(function (list) {
+    // decalre
+    let quoteText = document.createElement("li");
+    let authorText = document.createElement("li");
+    let datetext = document.createElement("li");
+
+    // content addition
+    quoteText.textContent = list.quote;
+    authorText.textContent = list.author;
+    datetext.textContent = list.date;
+
+    // appending element
+    quoteLi.appendChild(quoteText);
+    quoteLi.appendChild(authorText);
+    quoteLi.appendChild(datetext);
+  });
+}
+
+renderQuotes();
+
+// save quote to local storage button
 generateThoughtBtn.addEventListener("click", generateThoughtHandler);
---
+++ plan-and-update.md
@@ -0,0 +1,7 @@
+## Date 05/04/2026
+
+Planning to add;
+
+- Version History, Done
+- Save Quote to local storage, Done
+- A bit UI polish (Adding a skeletone/fade text for empty area of pre quote generation), Done
